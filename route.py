ae3b874369d153252c2c332df13efd99c550e07a
iwzy7071 <iwzy7071@gmail.com>
2019-12-29 10:28:04 +0800

    Teacher 1.增加查看课程分组 2.查看作业布置情况 3.查看教学计划功能 Student 1.增加查看作业连接
    其余bug已修改

--- route.py
+++ route.py
@@ -1,4 +1,8 @@
 from app import app
-
+from admin import route
+from teacher import route
+from student import route
+from login import route
+from forum import route
 if __name__ == '__main__':
     app.run()
